35e446b0b625f9fb5cc76972f4a09a6af688ac7b
Rafael Santos <osmarditto@gmail.com>
2023-07-19 21:29:46 -0300

    feat: Products list

---
+++ capputeeno/src/app/components/product-list.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { useProducts } from "../hooks/useProducts"
+
+interface ProductListProps {
+
+}
+export function ProductList(props: ProductListProps) {
+    const { data } = useProducts();
+    console.log(data);
+    return (
+        <></>
+    )
+}
